32c65317b8bde9959f033f5fb46ffca8f29b2f21
adele <adele.r87@gmail.com>
2021-01-28 15:12:01 +0100

    modifiche

--- js/main.js
+++ js/main.js
@@ -9,10 +9,13 @@ var app = new Vue({
 methods:{
 // creo funzione che al click mi inserisce una stringa di elementi in un array
 addElement:function() {
-  this.arraylist.push(this.elementi)
-}
+  this.arraylist.push(this.elementi);
+},
+deleteElement: function(index) {
+  this.arraylist.splice(index,1);
+},
 
-}
+},
 
 
 
